f9fb7d13180452a56f2028e99c5795fff3a55339
laura-ramos <ramoslaura418@gmail.com>
2024-10-20 20:52:41 -0600

    ipdate experience component

--- src/pages/Experience.tsx
+++ src/pages/Experience.tsx
@@ -1,79 +1,45 @@
+import { EXPERIENCE } from '../data/data';
+
 function Experience() {
 
     return (
         <>
-            <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
-                <li>
+        <div className="max-w-5xl mx-auto p-4">
+                        <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+                {EXPERIENCE.map((item, index) =>(
+                <li key={index}>
                     <div className="timeline-middle">
                         <svg
                             xmlns="http://www.w3.org/2000/svg"
                             viewBox="0 0 20 20"
                             fill="currentColor"
-                            className="h-5 w-5">
+                            className="h-7 w-7 text-accent">
                             <path
                                 fillRule="evenodd"
                                 d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
                                 clipRule="evenodd" />
                         </svg>
                     </div>
-                    <div className="timeline-start mb-10 md:text-end">
-                        <time className="font-mono italic">2022-Actualidad</time>
-                        <div className="text-lg font-black">Editiorial golfo pacifico</div>
-                        The Apple Macintosh—later rebranded as the Macintosh 128K—is the original Apple Macintosh
-                        personal computer. It played a pivotal role in establishing desktop publishing as a general
-                        office function. The motherboard.
+                    <div className={`card bg-base-100 shadow-xl mb-10 ${index % 2 === 0 ? 'timeline-start' : 'timeline-end'}`}>
+                    <div className="card-body">
+                    <h2 className="card-title">{item.title}</h2>
+                        <time className="font-mono italic">{item.start_date}-{item.end_date}</time>
+                        <div className="text-lg font-black">{item.company_name}</div>
+                        <ul className="list-disc">
+                                                    {item.description.map((data, index) => (
+                            <li>{data}</li>
+                        ))}
+                        </ul>
+
+                        </div>
                     </div>
-                    <hr />
+                    <hr className="bg-accent" />
                 </li>
-                <li>
-                    <hr />
-                    <div className="timeline-middle">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-5 w-5">
-                            <path
-                                fillRule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                clipRule="evenodd" />
-                        </svg>
-                    </div>
-                    <div className="timeline-end mb-10">
-                        <time className="font-mono italic">1998</time>
-                        <div className="text-lg font-black">SAPAO</div>
-                        iMac is a family of all-in-one Mac desktop computers designed and built by Apple Inc. It has
-                        been the primary part of Apple's consumer desktop offerings since its debut in August 1998,
-                        and has evolved through seven distinct forms
-                    </div>
-                    <hr />
-                </li>
-                <li>
-                    <hr />
-                    <div className="timeline-middle">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 20 20"
-                            fill="currentColor"
-                            className="h-5 w-5">
-                            <path
-                                fillRule="evenodd"
-                                d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                                clipRule="evenodd" />
-                        </svg>
-                    </div>
-                    <div className="timeline-start mb-10 md:text-end">
-                        <time className="font-mono italic">2019-2021</time>
-                        <div className="text-lg font-black">Freelance</div>
-                        The iPod is a discontinued series of portable media players and multi-purpose mobile devices
-                        designed and marketed by Apple Inc. The first version was released on October 23, 2001, about
-                        8+1⁄2 months after the Macintosh version of iTunes was released. Apple sold an estimated 450
-                        million iPod products as of 2022. Apple discontinued the iPod product line on May 10, 2022. At
-                        over 20 years, the iPod brand is the oldest to be discontinued by Apple
-                    </div>
-                    <hr />
-                </li>
+                ))}
+
             </ul>
+        </div>
+
         </>
     )
 }
